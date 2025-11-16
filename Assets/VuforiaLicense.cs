1dc916c01ea2fff0fbdc020203738d501316737c
ahmadzipur <ahmadzipur@gmail.com>
2025-11-16 09:55:38 +0700

    Upload

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QVNnMWg1Zi8vLy8vQUFBQm1WQkliN3JSc2tsWG1QajBjaElYNEdWd0FzWmZ0Y0piN1ZpTzl2aHRRSmx5RFExNEI2UjM1OFFpcDMvZlNiYXpGZHFlNVBIUC9ybkU2T3M1bmYrYmY1L291bFBCNHAyTGRIMDlJeDJMenltVGtlWnRlelNVVkp3L0xsTFpHTXlsYmZCMlNuaSttemxiQTY5QUxuMXdLZkx5Tk10b3J0RlZsUDV6Wkp5emRFbDZxeHBpeWJqVWNtTnprRW0yRFMzVXRuUVVaK0hMUnlmVWhHMzlYcU9BSlREOW9LQXRrRjdrMDZEMmY0dlFaNngyRWVsZHVTaUQvbGRZbGYzZFk0VGFiSnhlTTJZczJnTHNpNFl6TVJsSmpLQU5MdDRuZmdEY3Y2eTRNWVlHNUVZMlE3VUtxTkFpb1BHRllSWWovWUUzUzhwWWptZEhJb2wwQWVDV25STDlueG1iK0RoS01nZlFBbHpSN1JtMVZCUkY=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
---
+++ README.md
@@ -0,0 +1,2 @@
+# AR-Image-Tracker
+Proyek ini adalah aplikasi Augmented Reality berbasis Unity yang menggunakan fitur Image Tracking untuk menampilkan objek 3D ketika kamera mendeteksi gambar tertentu. Aplikasi ini memanfaatkan AR Foundation dan Vuforia untuk memberikan pengalaman AR yang stabil dan akurat.
